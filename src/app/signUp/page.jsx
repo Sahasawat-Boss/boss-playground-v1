af0808327643f51cd0bbb1c721d29ef6d7498da6
Boss <sahasawat.rk@gmail.com>
2025-01-04 07:17:20 +0700

    Create API Request send data to server

--- src/app/signUp/page.jsx
+++ src/app/signUp/page.jsx
@@ -1,80 +1,135 @@
 "use client"
 
+import React, { useState } from "react";
 import Container from "../Components/container";
 import NavBar from "../Components/nav";
 import Footer from "../Components/footer";
 import Link from 'next/link';
 
-
 function SignUp() {
-return (
-<Container>
-    <NavBar/>
-    <div 
-    className="hero flex-grow relative bg-cover bg-center pb-12"
-    style={{ 
-        backgroundImage: "url('https://plus.unsplash.com/premium_photo-1661963874418-df1110ee39c1?q=80&w=1086&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" 
-    }}
-    >
-    {/* Black overlay with 60% opacity */} {/* Add Animation: animate-floating and animate-fade-in-up*/}
-    <div className="absolute inset-0 bg-black bg-opacity-80"></div>
-    
-    {/* Hero Content */}
-    <div className="hero-content flex-col">
-        <div className="text-center text-white mx-auto max-w-3xl">
-        <h1 className="text-5xl font-bold animate-floating">Sign Up</h1>
-        <p className="pt-6 animate-fade-in-up">Join Boss Playground to unlock a world of creativity, collaboration, and innovation!</p>
-        <p className="pb-6 animate-fade-in-up">Create your account in just a few steps and start your journey with us!</p>
-        </div>
 
-        <div className="card w-[380px] shrink-0 bg-base-100 shadow-2xl">
-        {/*Form SignUp*/}
-        <form className="card-body pt-4 pb-4">
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [confirmPassword, setConfirmPassword] = useState("");
+const [error, setError] = useState("");
 
-            <div className="form-control">
-            <label className="label">
-                <span className="label-text">Name</span>
-            </label>
-            <input type="text" placeholder="Plese input your name" 
-            className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
-            </div>
-            <div className="form-control">
-            <label className="label">
-                <span className="label-text">Email</span>
-            </label>
-            <input type="email" placeholder="Plese input your email" 
-            className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
-            </div>
-            <div className="form-control">
-            <label className="label">
-                <span className="label-text">Password</span>
-            </label>
-            <input type="password" placeholder="Plese input your password" 
-            className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
-            </div>
-            <div className="form-control">
-            <label className="label">
-                <span className="label-text">Confirm Password</span>
-            </label>
-            <input type="password" placeholder="Plese re-input your password" 
-            className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
-            </div>
-            <div className="form-control mt-4">
+const handleSubmit = async (e) => {
+e.preventDefault(); // Prevent the form from refreshing the page
 
-            <button className="btn btn-primary px-16 py-3 text-white text-xl font-semibold bg-black shadow-md shadow-red-300 rounded-lg hover:bg-[#585858] hover:text-white hover:shadow-sky-500"  type='submit'>Sign Up</button>
-            
-            </div>
-            <p className="mt-3">Already have an account?
-            <Link href="/signIn" className='text-blue-500 font-medium hover:underline ml-1 '>Sign In</Link>
-            </p>
-        </form>
-        {/*Form SignUp*/}
-        </div>
-    </div>
-    </div>
-    <Footer/>
-</Container>
-)
+console.log("Name:", name); //Check if the input is captured
+console.log("Email:", email); //Check if the input is captured
+console.log("Password:", password); //Check if the input is captured
+console.log("Confirm Password:", confirmPassword); //Check if the input is captured
+
+if (password !== confirmPassword) {
+    console.log("Passwords do not match!");
+    setError("Password and Confirm Password do not match!");
+    return;
 }
+
+if (!name || !email || !password || !confirmPassword) {
+    console.log("Some fields are empty!");
+    setError("Please fill in all inputs!");
+    return;
+}
+
+try {
+        // ======= Send the data to the server (ยิง Request ส่งข้อมูลได้แล้ว) =======//
+        const res = await fetch("http://localhost:3000/api/signUp", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+        });
+
+        if (res.ok) {
+        const form = e.target; // Get the form element
+        setError(""); // Clear any previous error
+        form.reset(); // Reset the form
+        } else {
+        console.log("Registeration failed!");
+        } 
+
+    } catch (error) {
+        console.log("Error during sign up process: ", error);
+    }
+    // ======= Send the data to the server =======//
+}
+
+return (
+    <Container>
+        <NavBar/>
+        <div 
+        className="hero flex-grow relative bg-cover bg-center pb-12"
+        style={{ 
+            backgroundImage: "url('https://plus.unsplash.com/premium_photo-1661963874418-df1110ee39c1?q=80&w=1086&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" 
+        }}
+        >
+        {/* Black overlay with 60% opacity */} {/* Add Animation: animate-floating and animate-fade-in-up*/}
+        <div className="absolute inset-0 bg-black bg-opacity-80"></div>
+        
+        {/* Hero Content */}
+        <div className="hero-content flex-col">
+            <div className="text-center text-white mx-auto max-w-3xl">
+            <h1 className="text-5xl font-bold animate-floating">Sign Up</h1>
+            <p className="pt-6 animate-fade-in-up">Join Boss Playground to unlock a world of creativity, collaboration, and innovation!</p>
+            <p className="pb-6 animate-fade-in-up">Create your account in just a few steps and start your journey with us!</p>
+            </div>
+
+            <div className="card w-[380px] shrink-0 bg-base-100 shadow-2xl">
+            {/*Form SignUp*/}
+            <form onSubmit={handleSubmit} className="card-body pt-4 pb-4">
+
+                {error && (
+                <div className="flex justify-center">
+                    <div className="bg-red-500 w-fit text-white text-sm px-2 py-1 rounded-md">{error}</div>
+                </div>
+                )}
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Name</span>
+                </label>
+                <input type="text" onChange={(e)=>setName(e.target.value)} placeholder="Plese input your name" 
+                className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
+                </div>
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder="Plese input your email" 
+                className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
+                </div>
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder="Plese input your password" 
+                className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
+                </div>
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Confirm Password</span>
+                </label>
+                <input type="password" onChange={(e)=>setConfirmPassword(e.target.value)} placeholder="Plese re-input your password" 
+                className="input input-bordered focus:border-blue-500 focus:ring-2 focus:ring-blue-400"/>
+                </div>
+                <div className="form-control mt-4">
+                <button className="btn btn-primary px-16 py-3 text-white text-xl font-semibold bg-black shadow-md shadow-red-300 rounded-lg hover:bg-[#585858] hover:text-white hover:shadow-sky-500"  type='submit'>Sign Up</button>
+                </div>
+                <p className="mt-3">Already have an account?
+                <Link href="/signIn" className='text-blue-500 font-medium hover:underline ml-1 '>Sign In</Link>
+                </p>
+            </form>
+            {/*Form SignUp*/}
+            </div>
+        </div>
+        </div>
+        <Footer/>
+    </Container>
+    )
+    }
 
 export default SignUp
