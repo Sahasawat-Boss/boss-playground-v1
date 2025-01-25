ae77060b2d1b7d9ef0c40df5f52f8ddcd630d08f
Boss <sahasawat.rk@gmail.com>
2025-01-25 08:08:18 +0700

    Edit UI PI From

--- src/app/pg1/app2/piresquest/components/pirform.jsx
+++ src/app/pg1/app2/piresquest/components/pirform.jsx
@@ -47,13 +47,13 @@ const RequestForm = () => {
     return (
         <form
             onSubmit={handleSubmit}
-            className="max-w-7xl mx-auto bg-white dark:bg-transparent pt-2 pb-4 px-6 animate-fade-in"
+            className="max-w-7xl xl:border-x-2 mx-auto bg-white dark:bg-transparent pt-2 pb-4 px-6 animate-fade-in"
         >
             <div className='flex item-center justify-between'>
                 <h1 className="text-3xl text-black dark:text-white font-semibold animate-fade-in-right-left">
                     Process Inspection Request (PIR)
                 </h1>
-                <div className="mt-2 text-xl text-black dark:text-white hover:scale-150 animate-fade-in">
+                <div className="mt-2 text-xl text-black dark:text-white hover:text-yellow-500 dark:hover:text-yellow-400 hover:scale-150 drop-shadow-lg animate-fade-in">
                     <FaRegQuestionCircle />
                 </div>
             </div>
@@ -61,9 +61,9 @@ const RequestForm = () => {
             <hr className='border mt-2' />
             <h2 className="text-lg font-bold mt-4">Requestor</h2>
 
-            <div className="grid grid-cols-2 py-4 gap-x-6 xl:gap-x-20 gap-y-3">
+            <div className="grid grid-cols-2 py-4 gap-x-10 xl:gap-x-28 gap-y-3">
                 <div>
-                    <label className="block font-medium mb-1">
+                    <label className="block mb-1">
                         Project:
                         <select
                             type="text"
@@ -81,7 +81,7 @@ const RequestForm = () => {
                     </label>
                 </div>
                 <div>
-                    <label className="block font-medium mb-1">
+                    <label className="block mb-1">
                         Detected Date:
                         <input
                             type="date"
@@ -93,7 +93,7 @@ const RequestForm = () => {
                     </label>
                 </div>
                 <div>
-                    <label className="block font-medium">
+                    <label className="block">
                         Detected By:
                         <input
                             type="text"
@@ -106,7 +106,7 @@ const RequestForm = () => {
                     </label>
                 </div>
                 <div>
-                    <label className="block font-medium">
+                    <label className="block">
                         Detected Process:
                         <select
                             name="detectedProcess"
@@ -124,7 +124,7 @@ const RequestForm = () => {
                 </div>
             </div>
             <div className='mb-4'>
-                <span className="block font-medium mb-2">Severity Level:</span>
+                <span className="block mb-2">Severity Level:</span>
                 <div className="flex items-center space-x-4">
                     <label className="flex items-center">
                         <input
@@ -161,35 +161,66 @@ const RequestForm = () => {
                     </label>
                 </div>
             </div>
-            <div className='mb-4'>
-                <label className="block font-medium mb-1">
-                    Evidence or Attachment:
-                    <input
-                        type="file"
-                        name="evidence"
-                        onChange={handleFileChange}
-                        className="text-black dark:text-white w-full border border-gray-300 rounded px-3 py-4 mt-1"
-                    />
-                </label>
-            </div>
-            <div>
-                <label className="block font-medium mb-6">
-                    Details to support the evidence:
-                    <textarea
-                        name="details"
-                        value={formData.details}
-                        onChange={handleChange}
-                        placeholder="Write details to support the evidence..."
-                        className="text-black w-full h-32 border border-gray-300 rounded px-3 py-2 mt-1"
-                    />
-                </label>
+
+            <label className="block mb-2">
+                Upload evidence or attachment:
+            </label>
+
+            <div className="mb-4">
+                <div className="flex flex-col items-center justify-center w-full h-40 border-2 border-dashed border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                    {!formData.evidence ? (
+                        <>
+                            <p className=" text-gray-500 dark:text-gray-400 mb-2">Select Files Here</p>
+                            <p className="text-xs text-gray-500 dark:text-gray-400 mb-6">Files Supported: PDF, PNG, JPG, JPEG</p>
+                            <label
+                                htmlFor="multiple_files"
+                                className="text-sm px-6 py-1.5 bg-blue-700 text-white rounded-md cursor-pointer hover:bg-blue-400 focus:outline-none"
+                            >
+                                Choose Files
+                            </label>
+                            <input
+                                id="multiple_files"
+                                type="file"
+                                name="evidence"
+                                onChange={(e) => {
+                                    handleFileChange(e);
+                                    if (e.target.files && e.target.files.length > 0) {
+                                        setFormData({ ...formData, evidence: Array.from(e.target.files) });
+                                    }
+                                }}
+                                multiple
+                                className="hidden"
+                            />
+                        </>
+                    ) : (
+                        <ul className="flex flex-col items-center text-sm text-gray-500 dark:text-gray-400">
+                            {Array.from(formData.evidence).map((file, index) => (
+                                <li key={index} className="w-full items-start mb-1 text-sm text-black">
+                                    File {index + 1}: ({file.name}) 
+                                    <span className='text-xs text-gray-500'> | size: {(file.size / (1024 * 1024)).toFixed(2)} MB</span>
+                                </li>
+                            ))}
+                            <button
+                                onClick={() => setFormData({ ...formData, evidence: '' })}
+                                className=" w-fit px-4 py-1.5 bg-red-500 text-white rounded hover:bg-red-400"
+                            >
+                                Remove All
+                            </button>
+                        </ul>
+
+                    )}
+                </div>
             </div>
 
+
+
+
             <hr className='border' />
+
             <h2 className="text-lg font-bold mt-3 mb-4">Inspector (Optional)</h2>
-            <div className="grid grid-cols-2 gap-6 xl:gap-x-20">
+            <div className="grid grid-cols-2 gap-x-10 xl:gap-x-28">
                 <div>
-                    <label className="block font-medium mb-1">
+                    <label className="block mb-1">
                         Inspector Name:
                         <input
                             type="text"
@@ -202,7 +233,7 @@ const RequestForm = () => {
                     </label>
                 </div>
                 <div>
-                    <label className="block font-medium mb-1">
+                    <label className="block mb-1">
                         Due Date:
                         <input
                             type="date"
@@ -218,7 +249,7 @@ const RequestForm = () => {
                 <button
                     type="button"
                     onClick={handleClear}
-                    className=" border-slate-400 dark:border-slate-400 bg-slate-400 text-white shadow-md rounded-sm hover:bg-slate-500 px-8 py-2">
+                    className="border bg-white dark:bg-slate-200 text-black shadow-md rounded-sm hover:bg-slate-100 dark:hover:bg-slate-400 px-8 py-2">
                     Clear
                 </button>
                 <button
